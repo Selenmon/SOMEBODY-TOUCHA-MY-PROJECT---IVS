967c7318f3e20f70544405951ba700d71e56ee37
Silverwing373 <38539165+Silverwing373@users.noreply.github.com>
2018-04-22 17:00:59 +0200

    Add files via upload

---
+++ MathLib.h
@@ -0,0 +1,96 @@
+#ifndef MATHLIB_H_INCLUDED
+#define MATHLIB_H_INCLUDED
+
+
+
+#endif // MATHLIB_H_INCLUDED
+
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+
+double CalcAdd(double a,double b)
+{
+    double r = 0.0;
+    r = a + b;
+    return r;
+}
+
+double CalcSub(double a,double b)
+{
+    double r = 0.0;
+    r = a - b;
+    return r;
+}
+
+double CalcMul(double a,double b)
+{
+    double r = 0.0;
+    r =  a * b;
+    return r;
+}
+
+double CalcDiv(double a,double b)
+{
+    double r = 0.0;
+    r = a / b;
+    return r;
+}
+
+double CalcFac(double a)
+{
+    double r = 1.0;
+    double i = 0.0;
+    for(i = a, i > 0, i--)
+    {
+        r = r * i;
+    }
+    return r;
+}
+
+double CalcPow(double a,int b)
+{
+    double r = 0.0;
+    if(b = 0)
+    {
+        r = 1.0;
+        return r;
+    }
+    if(a = 0)
+    {
+        r = 0.0
+        return r;
+    }
+    else if (b < 0)
+    {
+        fprintf(stderr ,"Error,exponent is not a natural number!")
+        return 0;
+    }
+    else
+    {
+        r = pow(a,b);
+        return r;
+
+    }
+}
+
+double CalcNRT(double a, int b)
+{
+    double r = 0.0;
+    if(a < 0)
+    {
+        fprintf(stderr, "Cannot calculate root of a negative number!")
+    }
+    else
+    {
+        r = pow(a, 1/b)
+    }
+}
+
+double CalcAdditional(double a, int b)
+{
+    double r = 0.0;
+    r = log(x) / log(b);
+    return r;
+}
